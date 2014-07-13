5a5f3429df7a8dbe7c13579110c6bf072ad23585
root <root@raspberrypi.(none)>
2014-07-13 16:59:50 +0900

    全体的に見直し中

--- common_define.h
+++ common_define.h
@@ -1,31 +1,56 @@
 #ifndef COMMON_DEFINE_H
 #define COMMON_DEFINE_H
 
+#ifndef MAX_DEV_ADDRLEN
+#define MAX_DEV_ADDRLEN 32
+#endif
+
+#ifndef MAX_DEV_NAMELEN
+#define MAX_DEV_NAMELEN 32
+#endif
+
+#ifndef DEFAULT_CNTRL_ADDR
+#define DEFAULT_CNTRL_ADDR "localhost"
+#endif
+
+#ifndef DEFAULT_CNTRL_PORT
+#define DEFAULT_CNTRL_PORT "58000"
+#endif
+
+#ifndef MAX_CNTRL_REQ_BUFF
+#define MAX_CNTRL_REQ_BUFF 65535
+#endif
+
+#ifndef MAX_CNTRL_RES_BUFF
+#define MAX_CNTRL_RES_BUFF 65535
+#endif
+
+#ifndef MAX_CNTRL_LINE_BUFFER
+#define MAX_CNTRL_LINE_BUFFER 2048
+#endif
+
 #ifndef DEFAULT_EVENT_PRIORITY
 #define DEFAULT_EVENT_PRIORITY 50
 #endif
-#ifndef MAX_LINE_BUFFER
-#define MAX_LINE_BUFFER 2048
-#endif
+
 #ifndef MAX_TCP_LISTEN
 #define MAX_TCP_LISTEN 8
 #endif
 
-
-/* XXXX configarable */
 #ifndef READ_TIMEOUT
 #define READ_TIMEOUT 60
 #endif
+
 #ifndef WRITE_TIMEOUT
 #define WRITE_TIMEOUT 60
 #endif
-#ifndef SERVER_HOST
-#define SERVER_HOST "localhost"
-#endif
-#ifndef SERVER_PORT
-#define SERVER_PORT "50000"
-#endif
+
+#define MAX_FPLUG_DEVICE 7
+
+#define TYPE_DEV_NAME 1
+#define TYPE_DEV_ADDR 2
 
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
+
 #endif
--- fplugstatc.c
+++ fplugstatc.c
@@ -8,291 +8,225 @@
 #include <string.h>
 #include <errno.h>
 #include <getopt.h>
-#include <event.h>
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/rfcomm.h>
 
-#define MAX_FPLUG_DEVICE 7
-
-/* F-PLUG request */
-const char Temp[]={0x10, 0x81, 0x00, 0x00, 0x0e, 0xf0, 0x00, 0x00, 0x11, 0x00, 0x62, 0x01, 0xe0, 0x00};
-const char Humid[]={0x10, 0x81, 0x00, 0x00, 0x0e, 0xf0, 0x00, 0x00, 0x12, 0x00, 0x62, 0x01, 0xe0, 0x00};
-const char Illum[]={0x10, 0x81, 0x00, 0x00, 0x0e, 0xf0, 0x00, 0x00, 0x0D, 0x00, 0x62, 0x01, 0xe0, 0x00};
-const char RWatt[]={0x10, 0x81, 0x00, 0x00, 0x0e, 0xf0, 0x00, 0x00, 0x22, 0x00, 0x62, 0x01, 0xe2, 0x00};
+#include "common_macros.h"
+#include "common_define.h"
+#include "string_util.h"
 
 struct fplug_device {
-	const char *device_address;
-        struct sockaddr_rc saddr;
-	int sd;  
-	int connected;
+	int type;
+	const char *device_name[MAX_DEV_NAMELEN];
+	const char *device_address[MAX_DEV_ADDRLEN];
 };
 typedef struct fplug_device fplug_device_t;
 
-struct fplugd {
-	int check_status;
-	int foreground;
+struct fplugc {
+	int check_stat;
 	fplug_device_t fplug_device[MAX_FPLUG_DEVICE];
 	int device_count;
-	int available_count;
-        struct event_base *event_base;
+	const char *addr[NI_MAXHOST];
+	const char *port[NI_MAXSERV];
+	int sd;
         sigset_t sigmask;
-        struct event sigterm_event;
-        struct event sigint_event;
-	struct timeval timer_tv;
-        struct event timer_event; // test
+	char request_buff[MAX_CNTRL_REQ_BUFF];
+	size_t request_len;
+	char response_buff[MAX_CNTRL_RES_BUFF];
 };
-typedef struct fplugd fplugd_t;
+typedef struct fplugc fplugc_t;
 
 void
-initialize_fplugd(
-    fplugd_t *fplugd)
+initialize_fplugc(
+    fplugc_t *fplugc)
 {
-	int i;
-
-	memset(fplugd, 0, sizeof(fplugd_t));
-	for (i = 0; i < MAX_FPLUG_DEVICE; i++) {
-		fplugd->fplug_device[i].sd = -1;
-	}
+	memset(fplugc, 0, sizeof(fplugc_t));
+	fplugc->sd = -1;
+	fplugc->addr = DEFAULT_CNTRL_ADDR;
+	fplugc->port = DEFAULT_CNTRL_PORT;
 }
 
 static int
 parse_command_arguments(
     int argc,
     char *argv[],
-    fplugd_t *fplugd)
+    fplugc_t *fplugc)
 {
 	int opt;
 
-	while ((opt = getopt(argc, argv, "d:thiwF")) != -1) {
+	while ((opt = getopt(argc, argv, "d:n:s:p:thiwF")) != -1) {
 		switch (opt) {
 		case 'd':
-			fplugd->fplug_device[fplugd->device_count].device_address = optarg;
-			fplugd->device_count += 1;
+			fplugc->fplug_device[fplugc->device_count].type = TYPE_DEV_ADDR;
+			strlcpy(fplugc->fplug_device[fplugc->device_count].device_address, optarg, sizeof(fplugc->fplug_device[0].device_address));
+			fplugc->device_count += 1;
+			break;
+		case 'n':
+			fplugc->fplug_device[fplugc->device_count].type = TYPE_DEV_NAME;
+			strlcpy(fplugc->fplug_device[fplugc->device_count].device_name, optarg, sizeof(fplugc->fplug_device[0].device_name));
+			fplugc->device_count += 1;
+			break;
+		case 's':
+			strlcpy(fplugc->addr, optarg, sizeof(fplugc->addr));
+			break;
+		case 'p':
+			strlcpy(fplugc->port, optarg, sizeof(fplugc->port));
 			break;
 		case 't':
-			fplugd->check_status |= 0x01;
+			fplugc->check_stat |= 0x01;
                    	break;
 		case 'h':
-			fplugd->check_status |= 0x02;
+			fplugc->check_stat |= 0x02;
                    	break;
 		case 'i':
-			fplugd->check_status |= 0x04;
+			fplugc->check_stat |= 0x04;
                    	break;
 		case 'w':
-			fplugd->check_status |= 0x08;
+			fplugc->check_stat |= 0x08;
                    	break;
-		case 'F':
-			fplugd->foreground = 1;
-			break;
 		default:
-			fprintf(stderr, "Usage: %s [[-d <device address>]...] [-thiw]\n", argv[0]);
+			fprintf(stderr, "Usage: %s [[-d <device address>]...] [[-n <device name>]...] [-s port] [-p port] [-thiw]\n", argv[0]);
 			return 1;
 		}
 	}
-	if (fplugd->check_status == 0) {
-		fprintf(stderr, "Usage: %s [[-d <device address>]...] [-thiw]\n", argv[0]);
+	if (fplugc->check_stat == 0) {
+		fprintf(stderr, "Usage: %s [[-d <device address>]...] [[-n <device name>]...] [-s port] [-p port] [-thiw]\n", argv[0]);
 		return 1;
 	}
 
 	return 0;
 }
 
-static void
-terminate(
-    int fd,
-    short event,
-    void *args)
+static void 
+make_request(
+    fplugc_t *fplugc)
 {
-        fplugd_t *fplugd = args;
+	fplug_device_t *fplug_device;
+	int tmp_len;
+	int req_len = 0;
 
-        event_del(&fplugd->sigterm_event);
-        event_del(&fplugd->sigint_event);
-        event_del(&fplugd->timer_event);
-}
-
-static int
-connect_bluetooth(
-    fplug_device_t *fplug_device)
-{
-	fplug_device->sd = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-	fplug_device->saddr.rc_family = AF_BLUETOOTH;
-	fplug_device->saddr.rc_channel = (uint8_t)1;
-	str2ba(fplug_device->device_address, &fplug_device->saddr.rc_bdaddr);
-	if (connect(fplug_device->sd, (struct sockaddr *)&fplug_device->saddr, sizeof(fplug_device->saddr)) < 0) {
-		fprintf(stderr, "can not connect bluetooth (%s)\n", strerror(errno));
-		goto error;
+	if ((tmp_len = snprintf(fplugc->request, sizeof(fplugc->request), "stat -c %d", fplugc->check_stat)) < 0) {
+		return 1;
 	}
-	fplug_device->connected = 1;
-
-	return 0;
-
-error:
-	if (fplug_device->sd != -1) {
-		close(fplug_device->sd);
-		fplug_device->sd = -1;
-	}
-
-	return 1;
-}
-
-static void
-close_bluetooth(
-    fplug_device_t *fplug_device)
-{
-	if (!fplug_device->connected) {
-		return;
-	}
-	fplug_device->connected = 0;
-	if (fplug_device->sd != -1) {
-		close(fplug_device->sd);
-		fplug_device->sd = -1;
-	}
-}
-
-static int
-do_request(int fd, const char *request, size_t request_size, unsigned char *response, size_t response_size)
-{
-	int wlen = 0, tmp_wlen;
-	int rlen = 0, tmp_rlen;
-
-	while (wlen != request_size) {
-		tmp_wlen = write(fd, &request[wlen], request_size - wlen);
-		if (tmp_wlen < 0) {
-			fprintf(stderr, "can not write request\n");
+	req_len += tmp_len;
+	for (i = 0; i < fplugc->device_count; i++) {
+		fplug_device = &fplugc->fplug_device[i];
+		tmp_len = 0;
+		if (fplug_device->type == TYPE_DEV_NAME) {
+			tmp_len = snprintf(&fplugc->request[req_len], sizeof(fplugc->request) - req_len, " -n %s", fplug_device->name);
+		} else if (fplug_device->type == TYPE_DEV_ADDR) {
+			tmp_len = snprintf(&fplugc->request[req_len], sizeof(fplugc->request) - req_len, " -d %s", fplug_device->addr);
+		}
+		if (tmp_len < 0) {
 			return 1;
 		}
-		wlen += tmp_wlen;
+		req_len += tmp_len;
 	}
-	while (rlen != response_size) {
-		tmp_rlen = read(fd, &response[rlen], response_size - rlen);
-		if (rlen < 0) {
-			fprintf(stderr, "can not read request\n");
-			return 1;
-		}
-		rlen += tmp_rlen;
+	if ((tmp_len = snprintf(&fplugc->request[req_len], sizeof(fplugc->request) - req_len, "\r\n")) < 0) {
+		return 1;
 	}
+	fplugc->request_len = req_len;
 
 	return 0;
 }
 
 static int
-get_statistics(
-    fplug_device_t *fplug_device,
-    int check_status)
+connect_daemon(
+    int *sock,
+    const char *addr,
+    const char *port)
 {
-	unsigned char buf[32];
-	size_t buf_size;
-	int i;
-	int fd = fplug_device->sd;
+        int s = -1;
+        struct addrinfo hints, *res, *res0 = NULL;
+        int reuse_addr = 1;
+        int nodelay = 1;
+        char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
 
-	if (!fplug_device->connected) {
-		return 0;
-	}
+        ASSERT(sock != NULL);
+        ASSERT(addr != NULL);
+        ASSERT(port != NULL);
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = PF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        if (getaddrinfo(addr, port, &hints, &res0) != 0) {
+                fprintf(stderr, "failed in getaddrinfo\n");
+                goto last;
+        }
+        for (res = res0; res; res = res->ai_next) {
+                if ((s = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
+                        fprintf(stderr, "failed in create socket\n");
+                        s = -1;
+                        continue;
+                }
+                if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &reuse_addr, sizeof(reuse_addr))) {
+                        fprintf(stderr, "failed in set REUSEADDR\n");
+                        close(s);
+                        s = -1;
+                        continue;
+                }
+                if (setsockopt(s, SOL_TCP, TCP_NODELAY, &nodelay, sizeof(nodelay))) {
+                        fprintf(stderr, "failed in set NODELAY\n");
+                        close(s);
+                        s = -1;
+                        continue;
+                }
+                if (getnameinfo(
+                    res->ai_addr,
+                    res->ai_addrlen,
+                    hbuf, sizeof(hbuf),
+                    sbuf, sizeof(sbuf),
+                    NI_NUMERICHOST|NI_NUMERICSERV)) {
+                        fprintf(stderr, "failed in getnameinfo %m\n");
+                } else {
+                        fprintf(stderr, "connect address = %s, port = %s\n", hbuf, sbuf);
+                }
+                if (connect(s, res->ai_addr, res->ai_addrlen) < 0) {
+                        fprintf(stderr, "failed in connect\n");
+                        close(s);
+                        s = -1;
+                        continue;
+                }
+                break;
+        }
+last:
+        if (res0) {
+                freeaddrinfo(res0);
+        }
+        if (s == -1) {
+                return 1;
+        }
+        *sock = s;
 
-	if (check_status & 0x01) {
-		i = 0;
-		while (1) {
-			buf_size = 15;
-			if (do_request(fd, Temp, sizeof(Temp), buf, buf_size)) {
-				printf("Temp 0 failed in request\n");
-				return 1;
-			}
-			if (buf[5] != 0x00) {
-				read(fd, &buf[buf_size], sizeof(buf) - buf_size);
-			}
-			if (buf[5] == 0x11 && buf[10] == 0x72 && buf[13] == 2) {
-				break;
-			}
-			i++;
-			if (i > 10) {
-				printf("Temp 0 give up to get statistics\n");
-				return 1;
-			}
-		}
-		printf("Temp %.1lf success\n", (double)((int)buf[14] + ((int)buf[15] << 8)) / 10.0);
-	}
-	if (check_status & 0x02) {
-		i = 0;
-		while (1) {
-			buf_size = 15;
-			if (do_request(fd, Humid, sizeof(Humid), buf, buf_size)) {
-				printf("Humid 0 failed in request\n");
-				return 1;
-			}
-			if (buf[5] != 0x00) {
-				read(fd, &buf[buf_size], sizeof(buf) - buf_size);
-			}
-			if (buf[5] == 0x12 && buf[10] == 0x72 && buf[13] == 1) {
-				break;
-			}
-			i++;
-			if (i > 10) {
-				printf("Humid 0 give up to get statistics\n");
-				return 1;
-			}
-		}
-		printf("Humid %d success\n", (int)buf[14]);
-	}
-	if (check_status & 0x04) {
-		i = 0;
-		while (1) {
-			buf_size = 15;
-			if (do_request(fd, Illum, sizeof(Illum), buf, buf_size)) {
-				printf("Illum 0 failed in request\n");
-				return 1;
-			}
-			if (buf[5] != 0x00) {
-				read(fd, &buf[buf_size], sizeof(buf) - buf_size);
-			}
-			if (buf[5] == 0x0d && buf[10] == 0x72 && buf[13] == 2) {
-				break;
-			}
-			i++;
-			if (i > 10) {
-				printf("Illum 0 give up to get statistics\n");
-				return 1;
-			}
-		}
-                printf("Illum %d success\n", (int)buf[14] + ((int)buf[15] << 8));
-	}
-	if (check_status & 0x08) {
-		i = 0;
-		while (1) {
-			buf_size = 15;
-			if (do_request(fd, RWatt, sizeof(RWatt), buf, buf_size)) {
-				printf("Watt 0 failed in request\n");
-				return 1;
-			}
-			if (buf[5] != 0x00) {
-				read(fd, &buf[buf_size], sizeof(buf) - buf_size);
-			}
-                        if (buf[5] == 0x22 && buf[10] == 0x72 && buf[13] == 2) {
-				break;
-			}
-			i++;
-			if (i > 10) {
-				printf("Watt 0 give up to get statistics\n");
-				return 1;
-			}
-		}
-		printf("Watt %.1lf success\n", (double)((int)buf[14] + ((int)buf[15] << 8)) / 10);
-	}
-
-	return 0;
+        return 0;
 }
 
-void
-statistics_main(
-    int fd,
-    short event,
-    void *args)
+static int
+do_request(
+    int sd,
+    const char *request,
+    size_t request_size,
+    unsigned char *response,
+    size_t response_size)
 {
-        fplugd_t *fplugd = args;
-	int i;
+        int wlen = 0, tmp_wlen;
+        int rlen = 0, tmp_rlen;
 
-	for (i = 0; i < fplugd->device_count; i++) {
-		get_statistics(&fplugd->fplug_device[i], fplugd->check_status);
-	}
+        while (wlen != request_size) {
+                tmp_wlen = write(fd, &request[wlen], request_size - wlen);
+                if (tmp_wlen < 0) {
+                        fprintf(stderr, "can not write request\n");
+                        return 1;
+                }
+                wlen += tmp_wlen;
+        }
+        while (rlen != response_size) {
+                tmp_rlen = read(fd, &response[rlen], response_size - rlen);
+                if (rlen < 0) {
+                        fprintf(stderr, "can not read response\n");
+                        return 1;
+                }
+                rlen += tmp_rlen;
+        }
+
+        return 0;
 }
 
 int
@@ -301,54 +235,36 @@ main(
     char *argv[])
 {
 	int error = 0;
-	fplugd_t fplugd;
+	fplugc_t fplugc;
+	int req_len;
 	int i;
 
-	initialize_fplugd(&fplugd);
-	if (parse_command_arguments(argc, argv, &fplugd)) {
+	initialize_fplugc(&fplugc);
+	if (parse_command_arguments(argc, argv, &fplugc)) {
 		return 1;
 	}
-        if (!fplugd.foreground) {
-                if (daemon(1,1)) {
-                        fprintf(stderr, "faile in daemonaize");
-                        return 1;
-                }
-                setsid();
-        }
-	fplugd.event_base = event_init();
-	sigaddset(&fplugd.sigmask, SIGPIPE);
-	pthread_sigmask(SIG_SETMASK, &fplugd.sigmask, NULL);
-	signal_set(&fplugd.sigterm_event, SIGTERM, terminate, &fplugd);
-	event_base_set(fplugd.event_base, &fplugd.sigterm_event);
-	signal_add(&fplugd.sigterm_event, NULL);
-	signal_set(&fplugd.sigint_event, SIGINT, terminate, &fplugd);
-	event_base_set(fplugd.event_base, &fplugd.sigint_event);
-	signal_add(&fplugd.sigint_event, NULL);
-	for (i = 0; i < fplugd.device_count; i++) {
-		if (connect_bluetooth(&fplugd.fplug_device[i])) {
-			continue;
-		}
-		fplugd.available_count += 1;
+	sigemptyset(&fplugc.sigmask);
+	sigaddset(&fplugc.sigmask, SIGPIPE);
+	pthread_sigmask(SIG_BLOCK, &fplugc.sigmask, NULL);
+	if (make_request(&fplugc)) {
+		fprintf(stderr, "faile in create request");
+		error = 1
+		goto last;
 	}
-	if (fplugd.available_count == 0) {
-		fprintf(stderr, "no there available device.");
+	if (connect_daemon(&fplugc.sd, fplugc.addr, fplugc.port)) {
+		fprintf(stderr, "faile in connect to daemon");
+		error = 1
+		goto last;
+	}
+	if (do_request(fplugc.sd, fplugc.request, fplugc.request_len, fplugc.response, sizeof(fplugc.response)) {
+		fprintf(stderr, "faile in connect to daemon");
 		error = 1;
 		goto last;
 	}
-	fplugd.timer_tv.tv_sec = 5;
-	fplugd.timer_tv.tv_usec = 0;
-	//evtimer_set(&fplugd.timer_event, statistics_main, &fplugd);
-	event_set(&fplugd.timer_event, -1, EV_PERSIST, statistics_main, &fplugd);
-	event_base_set(fplugd.event_base, &fplugd.timer_event);
-	evtimer_add(&fplugd.timer_event, &fplugd.timer_tv);
-	if (event_base_dispatch(fplugd.event_base) == -1) {
-		fprintf(stderr, "failed in event base dispatch");
-		error = 1;
-		goto last;
-	}
+
 last:
-	for (i = 0; i < fplugd.device_count; i++) {
-		close_bluetooth(&fplugd.fplug_device[i]);
+	if (fplugc.sd != -1) {
+		close(fplugc.sd);
 	}
 
 	return error;
