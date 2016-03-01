a6042b0e6de6be41e94d2fa944120384125c10d3
root <root@raspberrypi.(none)>
2016-03-01 16:42:24 +0900

    update

--- www/js/fplugstatd.js
+++ www/js/fplugstatd.js
@@ -2,3 +2,67 @@ $(document).ready(function(){
     $("#body-fplug-realtime").tabs();
     $("#body-fplug-hourly").tabs();
 });
+
+// あとはjavascriptかく
+
+/*
+
+// view切り替えイベント (デフォルトはリアルタイム)
+                        <li class="menu-child" id="sidebar-fplug-realtime" > リアルタイム情報</li>
+                        <li class="menu-child" id="sidebar-fplug-hourly"> 時間毎の情報 </li>
+                        <li class="menu-child" id="sidebar-fplug-realtime"> 制御 </li>
+
+
+
+// chart 表示 1
+                       <div id="body-fplug-realtime-temperature-fragment">
+                           <div id="body-fplug-realtime-temperature-chart">
+                               1
+                           </div>
+                       </div>
+                       <div id="body-fplug-realtime-humidity-fragment">
+                           <div id="body-fplug-realtime-humidity-chart">
+                               2
+                           </div>
+                       </div>
+                       <div id="body-fplug-realtime-illuminance-fragment">
+                           <div id="body-fplug-realtime-illuminance-chart">
+                               3
+                           </div>
+                       </div>
+                       <div id="body-fplug-realtime-watt-fragment">
+                           <div id="body-fplug-realtime-watt-chart">
+                               4
+                           </div>
+                       </div>
+
+                        
+// chart 表示 1
+                       <div id="body-fplug-hourly-temperature-fragment">
+                           <div id="body-fplug-hourly-temperature-chart">
+                               1
+                           </div>
+                       </div>
+                       <div id="body-fplug-hourly-humidity-fragment">
+                           <div id="body-fplug-realtime-humidity-chart">
+                               2
+                           </div>
+                       </div>
+                       <div id="body-fplug-hourly-illuminance-fragment">
+                           <div id="body-fplug-hourly-illuminance-chart">
+                               3
+                           </div>
+                       </div>
+                       <div id="body-fplug-hourly-watt-fragment">
+                           <div id="body-fplug-hourly-watt-chart">
+                               4
+                           </div>
+                       </div>
+// 制御イベント
+                    <div id="body-fplug-control">
+                        <button type="button" id="body-fplug-control-reset">リセット</button><br /><br />
+                        <button type="button" id="body-fplug-control-powerstart">電力取得開始</button><br /><br />
+                        <button type="button" id="body-fplug-control-datetimesetting">時刻設定</button><br /><br />
+                    </div>
+
+*/
